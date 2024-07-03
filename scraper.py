fad5f4418de345f9cd63a29aa84779c49c7f4795
antoniobob <bob.antonio@gmail.com>
2024-07-03 18:50:03 -0300

    atualizando calculadora

--- scraper.py
+++ scraper.py
@@ -1,37 +1,46 @@
+# scraper.py
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
-from bs4 import BeautifulSoup
+import re
 
-# Configuração inicial do Selenium
 service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service)
 
+def extract_image_url(style):
+    match = re.search(r'url\("([^"]+\.jpg)"\)', style)
+    if match:
+        return match.group(1)
+    return None
+
 try:
-    # Carrega a página
-    driver.get('https://www.natura.com.br/c/especial-primeira-compra?price_filter=100..149&sort=top-sellers')
-    # Dá um tempo para o JavaScript carregar
-    driver.implicitly_wait(10)
+    driver.get('https://www.natura.com.br/c/presentes-faixa-de-preco-agradecer?pageSize=48&price_filter=20..50&sort=top-sellers')
+    WebDriverWait(driver, 20).until(
+        EC.element_to_be_clickable((By.ID, "onetrust-accept-btn-handler"))
+    ).click()
+    print("Clicou no botão")
 
-    # Usa BeautifulSoup para analisar o HTML
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "natds127")))
+    product_cards = driver.find_elements(By.CLASS_NAME, "natds127")
+    print(f"Encontrados {len(product_cards)} cartões de produtos.")
 
-    # Encontrar especificamente a div com classe 'natds4'
-    target_div = soup.find('div', class_='natds4')
-    if target_div:
-        print("Div 'natds4' encontrada.")
-        # Imprimir uma versão mais detalhada do conteúdo interno
-        for child in target_div.find_all(recursive=True):  # Procura por todos os elementos internos
-            tag_name = child.name
-            class_list = ' '.join(child.get('class', []))
-            content_preview = child.get_text(strip=True, separator=' ')[0:100]  # Primeiros 100 caracteres
-            print(f"Elemento: {tag_name}, Classe: {class_list}, Conteúdo: {content_preview}")
-    else:
-        print("Div 'natds4' não encontrada.")
+    for card in product_cards:
+        image_div = card.find_element(By.CSS_SELECTOR, 'div[style*="background-image"]')
+        style = image_div.get_attribute('style')
+        image_url = extract_image_url(style)
+        print("URL da Imagem:", image_url)
+
+        #title_element = card.find_element(By.XPATH, ".//p[contains(@class, 'MuiBox-root natds676 natds95 natds675 natds665')]")
+        #print("Título do Produto:", title_element.text)
+
+        #price_element = card.find_element(By.XPATH, ".//h6[contains(@class, 'MuiTypography-root natds667 MuiTypography-subtitle2')]")
+        #print("Preço do Produto:", price_element.text)
+
+except Exception as e:
+    print(f"Erro encontrado: {str(e)}")
 
 finally:
-    # Fecha o navegador
     driver.quit()
-
-
-
